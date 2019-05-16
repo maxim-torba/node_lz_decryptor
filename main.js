381a5b840a347f1e90ec80f2629b27c8b491ee10
Maxim Torba <maximtorba@Maxims-MacBook-Pro.local>
2019-05-16 14:02:20 +0300

    get compressed str from argument 'compressed', add gitignore

--- main.js
+++ main.js
@@ -2,13 +2,39 @@ const args = require("args-parser")(process.argv);
 const fs = require('fs');
 let LZString = require('lz-string');
 
+let compressedStr = args.compressed;
+
+let decompressedStr = LZString.decompress(compressedStr);
+// console.log('decompressedStr', decompressedStr);
+
+fs.writeFileSync('decompressed.txt', decompressedStr);
+
+/*
 let path = args.file;
 console.log('path', path);
+*/
 
-let compressedStr = fs.readFileSync(path, 'utf-8');
-// console.log('compressedStr', compressedStr);
+/*
+let compressedStr = fs.readFileSync(path, 'UTF-8');
+console.log('compressedStr', compressedStr);
 
 let decompressedStr = LZString.decompress(compressedStr);
 console.log('decompressedStr', decompressedStr);
 
 fs.writeFileSync('decompressed.txt', decompressedStr);
+*/
+/*
+fs.readFile(path, 'utf8', (err, data) => {
+    if (err) throw err;
+    console.log('data', data);
+
+    let decompressedStr = LZString.decompress(data);
+    console.log('decompressedStr', decompressedStr);
+});*/
+
+/*
+let compressed = LZString.compress('fooo');
+console.log('compressed', compressed);
+let decompressed = LZString.decompress(compressed);
+console.log('decompressed', decompressed);
+*/
